2a72a370c1c70173683dce45215292542a8fae39
farelrn <farel@karadigital.co>
2023-07-03 21:47:28 +0700

    init

--- mini_wallet/urls.py
+++ mini_wallet/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    # path("admin/", admin.site.urls),
     path("wallet/", include("wallet.urls")),
+    # path("api/v1/", include("api.urls")),
 ]
--- wallet/views.py
+++ wallet/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the wallet index.")
+
+def init(request):
+    print(request)
+    return HttpResponse("init response")
